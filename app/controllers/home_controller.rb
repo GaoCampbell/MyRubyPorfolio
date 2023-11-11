704723541b6bc9d7b0eca9efe71bf7e010c2bbe6
jovancamp007@gmail.com <29214933+CampbellGhost@users.noreply.github.com>
2023-11-11 17:57:38 +0800

    updating cache pro

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,13 @@
 class HomeController < ApplicationController
   def index
   end
-  def cache_static_image_files
-    
-
+  
+  def set_cookie
+    cookies[:user_id] = {
+      value: '1234',
+      expires: 1.year.from_now,
+      httponly: true
+    }
+    render plain: 'Cookie set!'
   end
 end
